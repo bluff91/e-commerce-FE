67eb52577556ac080cda1f3413be65deff9929ee
bluff <bluffyx91@gmail.com>
2023-05-25 18:54:56 +0300

    displaying cart data in the cart page; stylying not complete yet

--- src/components/CartColumns.jsx
+++ src/components/CartColumns.jsx
@@ -1,4 +1,18 @@
+import './CSS/CartColumns.css'
+
 const CartColumns = () => {
-  return <div>CartColumns</div>
+  return (
+    <div className="cart-columns">
+      <div className="cart-content">
+        <h5>item</h5>
+        <h5>price</h5>
+        <h5>quntity</h5>
+        <h5>subtotal</h5>
+        <span></span>
+      </div>
+      <hr />
+    </div>
+  )
 }
+
 export default CartColumns
